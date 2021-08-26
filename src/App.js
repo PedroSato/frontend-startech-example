82958b33781227a033cac3ad3940f812eab5bd7d
Pedro Sato <pedro.sato@gamersafer.com>
2021-08-26 14:18:02 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import axios from 'axios';
 
-function App() {
+const FormInput = styled.input`
+border: 1px solid red;
+
+`;
+const ErrorSpan = styled.span`
+  color: red;
+  display: ${(props) => props.isError ? 'block' : 'none'};
+`;
+
+
+const App = () => {
+
+  const fetchAddress = async () => {
+    const address = await axios.get(`https://viacep.com.br/ws/${form.cep}/json/`);
+    setForm({ ...form, logradouro: address.data.logradouro });
+  };
+
+  const createCandidate = async (candidate) => {
+    try {
+      const user = await axios.post('http://localhost:5000/register', form);
+      if (user.status === 200) {
+        alert('iti malia deu certo');
+      }
+
+    } catch (error) {
+      setCpfError(true);
+    }
+  };
+
+  const [form, setForm] = useState({
+    name: '',
+    cep: '',
+    email: '',
+    gender: '',
+  });
+
+  const [cpfError, setCpfError] = useState(false);
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <label>nome</label>
+        <FormInput onChange={(e) => {
+          setForm({ ...form, name: e.target.value });
+        }} value={form.name}></FormInput>
+      </div>
+      <div>
+        <label>cep</label>
+        <FormInput onBlur={() => {
+          fetchAddress();
+        }} onChange={(e) => {
+          setForm({ ...form, cep: e.target.value });
+        }} value={form.cep}></FormInput>
+      </div>
+      <div>
+        <label>email</label>
+        <FormInput onChange={(e) => {
+          setForm({ ...form, email: e.target.value });
+        }} value={form.email}></FormInput>
+        <ErrorSpan isError={cpfError}>AAAAAAAAAAAAAAAAAHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH</ErrorSpan>
+
+      </div>
+
+      <div>
+        <label>gênero</label>
+        <FormInput onChange={(e) => {
+          setForm({ ...form, gender: e.target.value });
+        }} value={form.gender}></FormInput>
+      </div>
+
+      <button onClick={() => createCandidate()}>Jesus apaga a luz</button>
     </div>
+
   );
-}
+};
 
 export default App;
